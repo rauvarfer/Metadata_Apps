867082daec5741efb3e800c87f5c47dfdb99d942
rauvarfer <rvarela@us.es>
2024-12-03 14:29:26 +0100

    Fixing test

--- ToF-ERDA-app/src/App.test.js
+++ ToF-ERDA-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders ToF-ERDA Metadata Generator text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/ToF-ERDA Metadata Generator/i);
   expect(linkElement).toBeInTheDocument();
 });
