cdc58eb2d1889fec7e35b34a8106d8d08a076f7a
rauvarfer <rvarela@us.es>
2024-12-03 14:26:04 +0100

    fixing test

--- IBA-app/src/App.test.js
+++ IBA-app/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders IBA Metadata Generator text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/IBA Metadata Generator/i);
   expect(linkElement).toBeInTheDocument();
 });
+
