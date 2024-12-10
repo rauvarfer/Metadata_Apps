1f9a7c93ef5c4cc6cffe75e51cd87dd2a2f2cf58
rauvarfer <rvarela@us.es>
2024-12-10 14:52:41 +0100

    Fixing tab change in ToF-ERDA, and minor things in IBA

--- ToF-ERDA-app/src/App.js
+++ ToF-ERDA-app/src/App.js
@@ -114,7 +114,7 @@ function App() {
           if (activeTab === 'General Info.') {
             setActiveTab('Experiment Setup');
           } else {
-            setActiveTab('General Info.n');
+            setActiveTab('General Info.');
           }
 
           // Llamar a la API de Electron para minimizar y maximizar la ventana
@@ -163,9 +163,6 @@ function App() {
     if (window.electron) {
         window.electron.minimizeAndRestore();
       }
-
-    setActiveTab('Sample');
-    setActiveTab('Experiment Setup');
 
   };
 
